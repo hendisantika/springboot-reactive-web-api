6d3e885b49237fc9e932a61bdf7d02b821b94f58
hendisantika <hendisantika@yahoo.co.id>
2019-06-28 06:22:32 +0700

    #4 - Add ErrorCode

---
+++ src/main/java/com/hendisantika/springbootreactivewebapi/entity/ErrorCode.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootreactivewebapi.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reactive-web-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-28
+ * Time: 06:22
+ */
+public enum ErrorCode {
+    ENTITY_NOT_FOUND
+}
