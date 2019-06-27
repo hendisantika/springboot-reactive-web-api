e590c027e0e1fc4643c7f626a253227ef9653a4d
hendisantika <hendisantika@yahoo.co.id>
2019-06-28 06:28:52 +0700

    #10 - Add UpdateBookRequest

---
+++ src/main/java/com/hendisantika/springbootreactivewebapi/dto/UpdateBookRequest.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootreactivewebapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reactive-web-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-28
+ * Time: 06:28
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateBookRequest {
+    private String id;
+    private String title;
+}
