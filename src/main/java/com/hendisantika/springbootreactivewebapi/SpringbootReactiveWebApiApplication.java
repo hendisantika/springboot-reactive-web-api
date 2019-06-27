71d91f0a6128e999f0d3f1a0eb91d5495f1fdaf7
hendisantika <hendisantika@yahoo.co.id>
2019-06-28 06:17:02 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springbootreactivewebapi/SpringbootReactiveWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootreactivewebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactiveWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootReactiveWebApiApplication.class, args);
+    }
+
+}
