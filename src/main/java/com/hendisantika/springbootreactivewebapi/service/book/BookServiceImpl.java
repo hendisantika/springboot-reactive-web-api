a311af89ba7e4fbe5378fc4fbba83f0b3939d44f
hendisantika <hendisantika@yahoo.co.id>
2019-06-29 06:31:56 +0700

    #16 - Add BookServiceImpl

---
+++ src/main/java/com/hendisantika/springbootreactivewebapi/service/book/BookServiceImpl.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootreactivewebapi.service.book;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reactive-web-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-29
+ * Time: 06:31
+ */
+@Service
+public class BookServiceImpl implements BookService {
+}
