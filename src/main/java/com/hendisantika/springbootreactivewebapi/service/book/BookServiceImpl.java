833161d791c17c4f3fe2a0b037e69422ad1bc8f3
hendisantika <hendisantika@yahoo.co.id>
2019-06-29 06:32:32 +0700

    #17 - Add saveBookToRepository on BookServiceImpl

--- src/main/java/com/hendisantika/springbootreactivewebapi/service/book/BookServiceImpl.java
+++ src/main/java/com/hendisantika/springbootreactivewebapi/service/book/BookServiceImpl.java
@@ -1,6 +1,15 @@
 package com.hendisantika.springbootreactivewebapi.service.book;
 
+import com.hendisantika.springbootreactivewebapi.dto.request.AddBookRequest;
+import com.hendisantika.springbootreactivewebapi.entity.Author;
+import com.hendisantika.springbootreactivewebapi.repository.AuthorRepository;
+import com.hendisantika.springbootreactivewebapi.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import rx.Single;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,4 +22,26 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Override
+    public Single<String> addBook(AddBookRequest addBookRequest) {
+        return saveBookToRepository(addBookRequest);
+    }
+
+    private Single<String> saveBookToRepository(AddBookRequest addBookRequest) {
+        return Single.create(singleSubscriber -> {
+            Optional<Author> optionalAuthor = authorRepository.findById(addBookRequest.getAuthorId());
+            if (!optionalAuthor.isPresent())
+                singleSubscriber.onError(new EntityNotFoundException());
+            else {
+                String addedBookId = bookRepository.save(toBook(addBookRequest)).getId();
+                singleSubscriber.onSuccess(addedBookId);
+            }
+        });
+    }
 }
