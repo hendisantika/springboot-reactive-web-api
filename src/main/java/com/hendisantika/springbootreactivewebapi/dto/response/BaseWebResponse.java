51dadb78e557bcb4939e490d59c3dc3aae9a51e6
hendisantika <hendisantika@yahoo.co.id>
2019-06-29 06:38:59 +0700

    #26 - Add BaseWebResponse on BookServiceImpl

---
+++ src/main/java/com/hendisantika/springbootreactivewebapi/dto/response/BaseWebResponse.java
@@ -0,0 +1,38 @@
+package com.hendisantika.springbootreactivewebapi.dto.response;
+
+import com.hendisantika.springbootreactivewebapi.entity.ErrorCode;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reactive-web-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-29
+ * Time: 06:38
+ */
+@Getter
+@Builder
+public class BaseWebResponse<T> {
+    private ErrorCode errorCode;
+    private T data;
+
+    public static BaseWebResponse successNoData() {
+        return BaseWebResponse.builder()
+                .build();
+    }
+
+    public static <T> BaseWebResponse<T> successWithData(T data) {
+        return BaseWebResponse.<T>builder()
+                .data(data)
+                .build();
+    }
+
+    public static BaseWebResponse error(ErrorCode errorCode) {
+        return BaseWebResponse.builder()
+                .errorCode(errorCode)
+                .build();
+    }
+}
