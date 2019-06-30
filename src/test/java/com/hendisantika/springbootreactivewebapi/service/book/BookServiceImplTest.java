7ad206c32a1cefbc86e9e2a579c5d40799f493f7
hendisantika <hendisantika@yahoo.co.id>
2019-06-30 15:08:27 +0700

    #31 - Add AddBook_Success_ReturnSingleOfAddedBookId test case in BookServiceImplTest

--- src/test/java/com/hendisantika/springbootreactivewebapi/service/book/BookServiceImplTest.java
+++ src/test/java/com/hendisantika/springbootreactivewebapi/service/book/BookServiceImplTest.java
@@ -1,11 +1,23 @@
 package com.hendisantika.springbootreactivewebapi.service.book;
 
+import com.hendisantika.springbootreactivewebapi.dto.request.AddBookRequest;
+import com.hendisantika.springbootreactivewebapi.entity.Author;
+import com.hendisantika.springbootreactivewebapi.entity.Book;
 import com.hendisantika.springbootreactivewebapi.repository.AuthorRepository;
 import com.hendisantika.springbootreactivewebapi.repository.BookRepository;
 import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,6 +41,25 @@ public class BookServiceImplTest {
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void AddBook_Success_ReturnSingleOfAddedBookId() {
+        when(authorRepository.findById(anyString()))
+                .thenReturn(Optional.of(new Author("1", "1")));
+        when(bookRepository.save(any(Book.class)))
+                .thenReturn(new Book("1", "1", new Author()));
+
+        bookService.addBook(new AddBookRequest("1", "1"))
+                .test()
+                .assertComplete()
+                .assertNoErrors()
+                .assertValue("1")
+                .awaitTerminalEvent();
+
+        InOrder inOrder = inOrder(authorRepository, bookRepository);
+        inOrder.verify(authorRepository, times(1)).findById(anyString());
+        inOrder.verify(bookRepository, times(1)).save(any(Book.class));
     }
 
 
