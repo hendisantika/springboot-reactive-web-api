5a52d7854c4196754586490a8122ba57697d4db2
hendisantika <hendisantika@yahoo.co.id>
2019-07-02 06:57:28 +0700

    #45 - Add AddBook_Success_Return201 in BookRestControllerTest

--- src/test/java/com/hendisantika/springbootreactivewebapi/controller/BookRestControllerTest.java
+++ src/test/java/com/hendisantika/springbootreactivewebapi/controller/BookRestControllerTest.java
@@ -1,13 +1,27 @@
 package com.hendisantika.springbootreactivewebapi.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.springbootreactivewebapi.dto.request.AddBookRequest;
+import com.hendisantika.springbootreactivewebapi.dto.request.AddBookWebRequest;
 import com.hendisantika.springbootreactivewebapi.service.book.BookService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import rx.Single;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,5 +44,23 @@ public class BookRestControllerTest {
 
     @MockBean
     private BookService bookService;
+
+    @Test
+    public void AddBook_Success_Return201() throws Exception {
+        when(bookService.addBook(any(AddBookRequest.class)))
+                .thenReturn(Single.just("1"));
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/books")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(new AddBookWebRequest())))
+                .andReturn();
+
+        mockMvc.perform(asyncDispatch(mvcResult))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.errorCode", nullValue()))
+                .andExpect(jsonPath("$.data", nullValue()));
+
+        verify(bookService, times(1)).addBook(any(AddBookRequest.class));
+    }
 
 }
