b384cc8deadc0c760bb7b83dd7df66434d73fa51
hendisantika <hendisantika@yahoo.co.id>
2019-07-02 06:54:40 +0700

    #43 - Add AddAuthor_Success_Return201 Test Case in AuthorRestControllerTest

--- src/test/java/com/hendisantika/springbootreactivewebapi/controller/AuthorRestControllerTest.java
+++ src/test/java/com/hendisantika/springbootreactivewebapi/controller/AuthorRestControllerTest.java
@@ -1,13 +1,27 @@
 package com.hendisantika.springbootreactivewebapi.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.springbootreactivewebapi.dto.request.AddAuthorRequest;
+import com.hendisantika.springbootreactivewebapi.dto.request.AddAuthorWebRequest;
 import com.hendisantika.springbootreactivewebapi.service.author.AuthorService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import rx.Single;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,5 +44,23 @@ public class AuthorRestControllerTest {
 
     @MockBean
     private AuthorService authorService;
+
+    @Test
+    public void AddAuthor_Success_Return201() throws Exception {
+        when(authorService.addAuthor(any(AddAuthorRequest.class)))
+                .thenReturn(Single.just("1"));
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/authors")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(new AddAuthorWebRequest("1"))))
+                .andReturn();
+
+        mockMvc.perform(asyncDispatch(mvcResult))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.errorCode", nullValue()))
+                .andExpect(jsonPath("$.data", nullValue()));
+
+        verify(authorService, times(1)).addAuthor(any(AddAuthorRequest.class));
+    }
 
 }
