b10cc3334247280bd34db8cfb83cb29c3eb656cf
hendisantika <hendisantika@yahoo.co.id>
2019-07-02 06:58:05 +0700

    #46 - Add AddBook_Failed_AuthorNotFound_Return404EntityNotFound in BookRestControllerTest

--- src/test/java/com/hendisantika/springbootreactivewebapi/controller/BookRestControllerTest.java
+++ src/test/java/com/hendisantika/springbootreactivewebapi/controller/BookRestControllerTest.java
@@ -3,6 +3,7 @@ package com.hendisantika.springbootreactivewebapi.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hendisantika.springbootreactivewebapi.dto.request.AddBookRequest;
 import com.hendisantika.springbootreactivewebapi.dto.request.AddBookWebRequest;
+import com.hendisantika.springbootreactivewebapi.entity.ErrorCode;
 import com.hendisantika.springbootreactivewebapi.service.book.BookService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +16,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import rx.Single;
 
+import javax.persistence.EntityNotFoundException;
+
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -62,5 +66,24 @@ public class BookRestControllerTest {
 
         verify(bookService, times(1)).addBook(any(AddBookRequest.class));
     }
+
+    @Test
+    public void AddBook_Failed_AuthorNotFound_Return404EntityNotFound() throws Exception {
+        when(bookService.addBook(any(AddBookRequest.class)))
+                .thenReturn(Single.error(new EntityNotFoundException()));
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/books")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(new AddBookWebRequest())))
+                .andReturn();
+
+        mockMvc.perform(asyncDispatch(mvcResult))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.errorCode", equalTo(ErrorCode.ENTITY_NOT_FOUND.toString())))
+                .andExpect(jsonPath("$.data", nullValue()));
+
+        verify(bookService, times(1)).addBook(any(AddBookRequest.class));
+    }
+
 
 }
