0bb47e808c27505d4b0dd865e16bcbe5b7be3352
hendisantika <hendisantika@yahoo.co.id>
2019-07-01 19:33:23 +0700

    #36 - Add addBook method in BookRestController

--- src/main/java/com/hendisantika/springbootreactivewebapi/controller/BookRestController.java
+++ src/main/java/com/hendisantika/springbootreactivewebapi/controller/BookRestController.java
@@ -1,9 +1,21 @@
 package com.hendisantika.springbootreactivewebapi.controller;
 
+import com.hendisantika.springbootreactivewebapi.dto.request.AddBookRequest;
+import com.hendisantika.springbootreactivewebapi.dto.request.AddBookWebRequest;
+import com.hendisantika.springbootreactivewebapi.dto.response.BaseWebResponse;
 import com.hendisantika.springbootreactivewebapi.service.book.BookService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rx.Single;
+import rx.schedulers.Schedulers;
+
+import java.net.URI;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +32,20 @@ public class BookRestController {
 
     @Autowired
     private BookService bookService;
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Single<ResponseEntity<BaseWebResponse>> addBook(@RequestBody AddBookWebRequest addBookWebRequest) {
+        return bookService.addBook(toAddBookRequest(addBookWebRequest))
+                .subscribeOn(Schedulers.io())
+                .map(s -> ResponseEntity.created(URI.create("/api/books/" + s)).body(BaseWebResponse.successNoData()));
+    }
+
+    private AddBookRequest toAddBookRequest(AddBookWebRequest addBookWebRequest) {
+        AddBookRequest addBookRequest = new AddBookRequest();
+        BeanUtils.copyProperties(addBookWebRequest, addBookRequest);
+        return addBookRequest;
+    }
 }
